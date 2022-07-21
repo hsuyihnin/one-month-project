c61cf610f02449afcc4c995d2d2d850abb708156
hsuyihnin <hsuyihnin27@gmail.com>
2022-07-21 17:31:09 +0630

    one month proj

--- app.js
+++ app.js
@@ -1,22 +1,67 @@
-const btn = document.querySelector( '#button' );
-document.getElementById( "button" ).addEventListener( "click", function ()
-{
-    document.querySelector( ".modal" ).style.display = "block";
-     
-} )
-document.getElementById( "close" ).addEventListener( "click", function ()
-{
-    document.querySelector( ".modal" ).style.display = "none";
-} )
+const btn = document.querySelector("#button");
+document.getElementById("button").addEventListener("click", function () {
+  document.querySelector(".modal").style.display = "block";
+});
+document.getElementById("close").addEventListener("click", function () {
+  document.querySelector(".modal").style.display = "none";
+});
 
-document.getElementById( "submit" ).addEventListener( "click", function ()
+document.getElementById("submit").addEventListener("click", function () {
+  const name = document.querySelector("#name");
+  const password = document.querySelector("#password");
+  const email = document.querySelector("#email");
+  const greet = document.querySelector(".greet");
+  let nameInput = name.value;
+  greet.textContent = "Hello!" + " " + nameInput;
+  btn.innerHTML = "Log Out";
+});
+
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const slides = document.querySelectorAll(".carousel");
+let TotalSlides = slides.length;
+let position = 0;
+nextBtn.addEventListener("click", () => {
+  hideAll();
+  if (position === TotalSlides - 1) {
+    position = 0;
+  } else {
+    position++;
+  }
+  slides[position].classList.add("visible");
+});
+prevBtn.addEventListener("click", () => {
+  hideAll();
+  if (position === 0) {
+    position = TotalSlides - 1;
+  } else {
+    position--;
+  }
+  slides[position].classList.add("visible");
+});
+function hideAll() {
+  for (let slide of slides) {
+    slide.classList.remove("visible");
+  }
+}
+const showMore = document.querySelector( ".showmore" );
+const showmoreBtn = document.querySelector( ".showmore_btn" );
+function showmore ()
 {
-    const name = document.querySelector( '#name' );
-    const password = document.querySelector( '#password' );
-    const email = document.querySelector( '#email' );
-    const greet = document.querySelector( '.greet' );
-    let nameInput = name.value;
-    greet.textContent = "Hello!" + " " + nameInput;
-    btn.innerHTML= "Log Out";
-   
-} )
+    if ( showMore.style.display === 'none' )
+    {
+        showMore.style.display = 'block';
+        showmoreBtn.innerHTML = 'close';
+    } else
+    {
+        showMore.style.display = 'none';
+    showmoreBtn.innerHTML = 'more';
+    }
+    // if ( showMore.style.display === 'none' )
+    // {
+       
+    // } else
+    // {
+        
+    // }
+}
